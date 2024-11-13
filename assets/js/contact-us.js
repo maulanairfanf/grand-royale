9b37178e627fe0e1f7d84cb51f1bbc9460216264
maulanairfanf <maulanairfanf@gmail.com>
2024-11-13 08:54:39 +0700

    updated

--- assets/js/contact-us.js
+++ assets/js/contact-us.js
@@ -1,35 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-	// Ambil semua elemen dengan kelas 'contact-us'
 	const contactUsElements = document.querySelectorAll('.contact-us')
 
-	// Iterasi melalui setiap elemen 'contact-us'
 	contactUsElements.forEach(element => {
-		// Ambil path file JSON dari atribut data-json
 		const jsonFilePath = element.getAttribute('data-json')
 
-		// Ambil data dari file JSON
 		fetch(jsonFilePath)
 			.then(response => response.json())
 			.then(data => {
-				// Buat HTML untuk kontak
-				const contactsHTML = data.contacts
-					.map(
-						contact => `
-											<div class="col-lg-6">
-													<a href="https://wa.me/${contact.phone.replace('+', '')}" target="_blank">
-															<div class="item phone">
-																	<img src="${contact.image}" alt="Phone Icon" style="max-width: 52px" />
-																	<h6>${contact.phone}<br /><span>${contact.name}</span></h6>
-															</div>
-													</a>
-											</div>
-									`
-					)
-					.join('')
 
-				// Buat HTML untuk seluruh bagian
+
 				const contactSectionHTML = `
-									<div class="contact section" id="contact">
+									<div class="contact section" id="parallax">
 											<div class="container">
 													<div class="row">
 															<div class="col-lg-4 offset-lg-4">
@@ -41,7 +22,6 @@ document.addEventListener('DOMContentLoaded', function () {
 													</div>
 											</div>
 									</div>
-
 									<div class="contact-content">
 											<div class="container">
 													<div class="row">
@@ -58,12 +38,12 @@ document.addEventListener('DOMContentLoaded', function () {
 																	</div>
 															</div>
 													</div>
-
 											</div>
-									</div>`
+									</div>
+									`
 
-				// Masukkan HTML ke dalam elemen saat ini
 				element.innerHTML = contactSectionHTML
+
 			})
 			.catch(error => {
 				console.error('Error loading JSON data:', error)
